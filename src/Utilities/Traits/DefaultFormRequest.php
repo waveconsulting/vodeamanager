9c16ca86b5888ff9a2189e947458bf05864e07ce
Yudi Hertanto <yudi@vodea.id>
2020-05-13 15:01:16 +0700

    update entity namespace

--- src/Utilities/Traits/DefaultFormRequest.php
+++ src/Utilities/Traits/DefaultFormRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Arr;
 
 trait DefaultFormRequest
 {
-    protected $namespaceEntity = 'App\\Entities\\';
+    protected $entityNamespace = 'App\\Entities\\';
 
     public function authorize() {
         return true;
@@ -26,7 +26,7 @@ trait DefaultFormRequest
 
     public function getRules() {
         $className = Arr::last(explode('\\',get_class($this)));
-        $nameSpace = $this->namespaceEntity . preg_replace('/(CreateRequest|UpdateRequest)/','',$className);
+        $nameSpace = $this->entityNamespace . preg_replace('/(CreateRequest|UpdateRequest)/','',$className);
         $model = app($nameSpace);
 
         $model->setValidationRules($this->all(), @$this->id);
@@ -36,7 +36,7 @@ trait DefaultFormRequest
 
     public function getMessages() {
         $className = Arr::last(explode('\\',get_class($this)));
-        $nameSpace = $this->namespaceEntity . preg_replace('/(CreateRequest|UpdateRequest)/','',$className);
+        $nameSpace = $this->entityNamespace . preg_replace('/(CreateRequest|UpdateRequest)/','',$className);
         $model = app($nameSpace);
 
         $model->setValidationMessages($this->all(), @$this->id);
@@ -46,7 +46,7 @@ trait DefaultFormRequest
 
     public function getAttributes() {
         $className = Arr::last(explode('\\',get_class($this)));
-        $nameSpace = $this->namespaceEntity . preg_replace('/(CreateRequest|UpdateRequest)/','',$className);
+        $nameSpace = $this->entityNamespace . preg_replace('/(CreateRequest|UpdateRequest)/','',$className);
         $model = app($nameSpace);
 
         $model->setValidationAttributes($this->all(), @$this->id);
