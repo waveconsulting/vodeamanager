c31462fb64afb96f49c2bea9e7dc45e858739588
Yudi Hertanto <yudi@vodea.id>
2020-05-26 17:09:56 +0700

    add base view

---
+++ src/Utilities/Entities/BaseView.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Vodeamanager\Core\Utilities\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+use Vodeamanager\Core\Utilities\Traits\Searchable;
+
+abstract class BaseView extends Model
+{
+    use Searchable;
+
+    /**
+     * Columns and their priority in search results.
+     * Columns with higher values are more important.
+     * Columns with equal values have equal importance.
+     ** @var array
+     */
+    protected $searchable = [
+        'columns' => [],
+        'joins' => [],
+    ];
+
+    public function scopeCriteria($query, Request $request) {
+        $order = null;
+        $sorted = null;
+
+        if ($request->has('order_by')) {
+            $sorted = $request->get('sorted_by') == 'desc' ? 'desc' : 'asc';
+            $order = $request->get('order_by');
+        } else if (config('vodeamanager.entity.sorting_default.active', false)) {
+            $order = config('vodeamanager.entity.sorting_default.column', 'id');
+            $sorted = config('vodeamanager.entity.sorting_default.order', 'desc') == 'desc' ? 'desc' : 'asc';
+        }
+
+        $query->when($order && $sorted && Schema::hasColumn($this->getTable(),$order), function ($query) use ($order, $sorted) {
+            $query->orderBy($order, $sorted);
+        });
+    }
+
+    public function scopeFilter($query, Request $request) {}
+
+    public function hasMany($related, $foreignKey = null, $localKey = null)
+    {
+        $instance = $this->newRelatedInstance($related);
+
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new HasManySyncable(
+            $instance->newQuery(), $this, $instance->getTable().'.'.$foreignKey, $localKey
+        );
+    }
+
+}
