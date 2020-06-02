0b3a7405ae3a1670b393412049d0d259f28943f3
Yudi Hertanto <yudi@vodea.id>
2020-06-02 14:03:10 +0700

    add constant

--- src/Utilities/Constant.php
+++ src/Utilities/Constant.php
@@ -14,4 +14,52 @@ class Constant
         7 => 'Sunday'
     ];
 
+    const GENDER_TYPE_MALE = 'M';
+    const GENDER_TYPE_FEMALE = 'F';
+    const GENDER_TYPE_OTHER = 'O';
+
+    const GENDER_TYPE_OPTIONS = [
+        self::GENDER_TYPE_MALE => 'Male',
+        self::GENDER_TYPE_FEMALE => 'Female',
+        self::GENDER_TYPE_OTHER => 'Others'
+    ];
+
+    const MARITAL_STATUS_SINGLE = 'SINGLE';
+    const MARITAL_STATUS_MARRIED = 'MARRIED';
+    const MARITAL_STATUS_WIDOW = 'WIDOW';
+    const MARITAL_STATUS_WIDOWER = 'WIDOWER';
+    const MARITAL_STATUS_OTHER = 'OTHER';
+
+    const MARITAL_STATUS_OPTIONS = [
+        self::MARITAL_STATUS_SINGLE => 'Single',
+        self::MARITAL_STATUS_MARRIED => 'Married',
+        self::MARITAL_STATUS_WIDOW => 'Widow',
+        self::MARITAL_STATUS_WIDOWER => 'Widower',
+        self::MARITAL_STATUS_OTHER => 'Others'
+    ];
+
+    const BLOOD_TYPE_A = 'A';
+    const BLOOD_TYPE_B = 'B';
+    const BLOOD_TYPE_AB = 'AB';
+    const BLOOD_TYPE_O = 'O';
+
+    const BLOOD_TYPE_OPTIONS = [
+        self::BLOOD_TYPE_A,
+        self::BLOOD_TYPE_B,
+        self::BLOOD_TYPE_AB,
+        self::BLOOD_TYPE_O,
+    ];
+
+    const RELATIONSHIP_TYPE_SPOUSE = 'SPOUSE';
+    const RELATIONSHIP_TYPE_PARENT = 'PARENT';
+    const RELATIONSHIP_TYPE_CHILD = 'CHILD';
+    const RELATIONSHIP_TYPE_SIBLING = 'SIBLING';
+
+    const RELATIONSHIP_TYPE_OPTIONS = [
+        self::RELATIONSHIP_TYPE_SPOUSE => 'Spouse',
+        self::RELATIONSHIP_TYPE_PARENT => 'Parent',
+        self::RELATIONSHIP_TYPE_CHILD => 'Child',
+        self::RELATIONSHIP_TYPE_SIBLING => 'Sibling',
+    ];
+
 }
