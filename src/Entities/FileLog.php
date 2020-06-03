255d25d2c5f12dcf6ae99777c456bfc795dc01f5
Yudi Hertanto <yudi@vodea.id>
2020-06-03 13:05:55 +0700

    remove log

--- src/Entities/FileLog.php
+++ src/Entities/FileLog.php
@@ -24,14 +24,28 @@ class FileLog extends BaseEntity
             'required',
             'string',
         ],
-        'disk' => [
-            'required',
-            'string',
-        ]
     ];
 
     public function getUrlAttribute() {
         return Storage::disk($this->disk)->url($this->path);
+    }
+
+    public function setValidationRules(array $request = [], $id = null)
+    {
+        $this->validationRules['disk'] = [
+            'required',
+            'in:' . implode(array_keys(config('filesystems.disks', [])), ',')
+        ];
+
+        return $this;
+    }
+
+    public function setValidationMessages(array $request = [])
+    {
+        $this->validationMessages['disk.in'] = 'The selected :attribute must be in '
+            . implode(array_keys(config('filesystems.disks', [])), ',');
+
+        return $this;
     }
 
 }
