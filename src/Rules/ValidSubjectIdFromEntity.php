1269f195fb9f59b06271440f7bb88d46094d7ab5
Yudi Hertanto <yudi@vodea.id>
2020-06-08 15:29:54 +0700

    add valid subject id

---
+++ src/Rules/ValidSubjectIdFromEntity.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Vodeamanager\Core\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Arr;
+
+class ValidSubjectIdFromEntity implements Rule
+{
+    protected $request;
+    protected $message = 'The selected subject is invalid.';
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param array $request
+     */
+    public function __construct(array $request = [])
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (!$entity = Arr::get($this->request, 'entity')) return false;
+
+        return !!app($entity)::find($value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return __($this->message);
+    }
+}
