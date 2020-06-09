c48e35f1c99efcb0e6a295c4ea1edb7cf87bdc8f
Yudi Hertanto <yudi@vodea.id>
2020-06-09 14:49:49 +0700

    valid constant

--- src/Entities/NumberSetting.php
+++ src/Entities/NumberSetting.php
@@ -3,7 +3,9 @@
 namespace Vodeamanager\Core\Entities;
 
 use Illuminate\Support\Arr;
+use Symfony\Component\Mime\Test\Constraint\EmailHeaderSame;
 use Vodeamanager\Core\Rules\ValidEntity;
+use Vodeamanager\Core\Rules\ValidInConstant;
 use Vodeamanager\Core\Utilities\Constant;
 use Vodeamanager\Core\Utilities\Entities\BaseEntity;
 
@@ -35,14 +37,15 @@ class NumberSetting extends BaseEntity
         ];
 
         $this->validationRules['reset_type'] = [
-            'in:' . implode(array_keys(Constant::NUMBER_SETTING_RESET_TYPE_OPTIONS), ',')
+            'nullable',
+            new ValidInConstant(Constant::NUMBER_SETTING_RESET_TYPE_OPTIONS),
         ];
 
         $this->validationRules['number_setting_components'] = 'array';
         $this->validationRules['number_setting_components.*.sequence'] = 'required|distinct|integer|min:1';
         $this->validationRules['number_setting_components.*.type'] = [
             'required',
-            'in:' . implode(array_keys(Constant::NUMBER_SETTING_COMPONENT_TYPE_OPTIONS)),
+            new ValidInConstant(Constant::NUMBER_SETTING_COMPONENT_TYPE_OPTIONS),
         ];
 
         $numberSettingComponents = Arr::get($request, 'number_setting_components', []);
@@ -50,16 +53,16 @@ class NumberSetting extends BaseEntity
             foreach ($numberSettingComponents as $index => $numberSettingComponent) {
                 $rules = ['required'];
                 switch($numberSettingComponent['type']) {
-                    case Constant::NUMBER_SETTING_COMPONENT_YEAR:
+                    case Constant::NUMBER_SETTING_COMPONENT_TYPE_YEAR:
                         $rules[] = 'in:y,Y';
                         break;
-                    case Constant::NUMBER_SETTING_COMPONENT_MONTH:
+                    case Constant::NUMBER_SETTING_COMPONENT_TYPE_MONTH:
                         $rules[] = 'in:m,M,F,n';
                         break;
-                    case Constant::NUMBER_SETTING_COMPONENT_DAY:
+                    case Constant::NUMBER_SETTING_COMPONENT_TYPE_DAY:
                         $rules[] = 'in:d,D,j,l';
                         break;
-                    case Constant::NUMBER_SETTING_COMPONENT_COUNTER:
+                    case Constant::NUMBER_SETTING_COMPONENT_TYPE_COUNTER:
                         $rules[] = 'integer';
                         $rules[] = 'min:1';
                         break;
@@ -68,14 +71,6 @@ class NumberSetting extends BaseEntity
                 $this->validationRules["number_setting_components.{$index}.format"] = $rules;
             }
         }
-
-        return $this;
-    }
-
-    public function setValidationMessages(array $request = [])
-    {
-        $this->validationMessages['reset_type.in'] = 'The selected :attribute must be in ' . implode(array_values(Constant::NUMBER_SETTING_RESET_TYPE_OPTIONS), ', ');
-        $this->validationMessages['number_setting_components.*.type.in'] = 'The selected :attribute must be in ' . implode(array_values(Constant::NUMBER_SETTING_COMPONENT_TYPE_OPTIONS), ', ');
 
         return $this;
     }
