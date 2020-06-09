0a948e159198b1cf830e402a7655e7cbd0b10594
Yudi Hertanto <yudi@vodea.id>
2020-06-09 15:33:07 +0700

    adjust

--- src/Entities/NumberSetting.php
+++ src/Entities/NumberSetting.php
@@ -3,6 +3,7 @@
 namespace Vodeamanager\Core\Entities;
 
 use Illuminate\Support\Arr;
+use Vodeamanager\Core\Rules\ValidEntity;
 use Vodeamanager\Core\Rules\ValidInConstant;
 use Vodeamanager\Core\Utilities\Constant;
 use Vodeamanager\Core\Utilities\Entities\BaseEntity;
@@ -32,6 +33,7 @@ class NumberSetting extends BaseEntity
         $this->validationRules['entity'] = [
             'required',
             'unique:number_settings,entity,' . ($id ?? 'NULL') . ',id,deleted_at,NULL',
+            new ValidEntity(),
         ];
 
         $this->validationRules['reset_type'] = [
---
+++ src/Rules/ValidEntity.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Vodeamanager\Core\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidEntity implements Rule
+{
+    protected $message;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param string $message
+     */
+    public function __construct($message = 'The selected :attribute is invalid.')
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return class_exists($value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return __($this->message);
+    }
+}
