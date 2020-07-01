d9bd02a1f0e99f00a966c5f8ca4d05ff8b72a6f6
Yudi Hertanto <yudi@vodea.id>
2020-07-01 11:34:00 +0700

    adjust

--- assets/seeds/GateSettingsTableSeeder.php
+++ assets/seeds/GateSettingsTableSeeder.php
@@ -11,6 +11,34 @@ class GateSettingsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $gateSettings = [];
+
+        foreach (config('vodeamanager.models.role')::where('is_special', 0)->pluck('code') as $roleCode) {
+            array_push($gateSettings, [
+                'role_code' => $roleCode,
+                'permission_ids' => config('vodeamanager.models.permission')::pluck('id'),
+            ]);
+        }
+
+        foreach ($gateSettings as $gateSetting) {
+            $permissionIds = $gateSetting['permission_ids'];
+            unset($gateSetting['permission_ids']);
+
+            if (isset($gateSetting['role_code'])) {
+                $role = config('vodeamanager.models.role')::where('code', $gateSetting['role_code'])->firstOrFail();
+                $gateSetting['role_id'] = $role->id;
+                unset($gateSetting['role_code']);
+            }
+
+            if (@$gateSetting['email']) {
+                $user = config('vodeamanager.models.user')::where('email', @$gateSetting['email'])->firstOrFail();
+                $gateSetting['user_id'] = $user->id;
+                unset($gateSetting['email']);
+            }
+
+            $gateSettings = config('vodeamanager.models.gate_setting')::create($gateSetting);
+
+            $gateSettings->permissions()->sync($permissionIds);
+        }
     }
 }
