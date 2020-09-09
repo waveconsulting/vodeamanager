47d370d06054d77a42b83931a61ba68d8580c6a5
Yudi Hertanto <yudi@vodea.id>
2020-09-09 10:33:54 +0700

    add create_from_date

---
+++ src/Utilities/Helpers/Date.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!function_exists('create_from_date')) {
+    /**
+     * @param $year
+     * @param $month
+     * @param $day
+     * @param string $roundType ceil|floor
+     *
+     * @return string
+     */
+    function create_from_date($year, $month, $day, $roundType = 'ceil') {
+        $date = new DateTime("$year-$month-01");
+        if ($day > 28 && $day > $date->format('t')) {
+            if ($roundType == 'floor') {
+                return $date->format('Y-m-') . $date->format('t');
+            } else if ($roundType == 'ceil') {
+                $date->modify('+1 month');
+                return $date->format('Y-m-01');
+            }
+        }
+
+        return (new DateTime("$year-$month-$day"))
+            ->format('Y-m-d');
+    }
+
+}
