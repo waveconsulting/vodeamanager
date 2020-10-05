aebceb4fadebace0e5984b2e6e4988a84615a397
Yudi Hertanto <yudi@vodea.id>
2020-10-05 17:50:14 +0700

    add date time converter

--- src/Utilities/Helpers/Date.php
+++ src/Utilities/Helpers/Date.php
@@ -23,5 +23,46 @@ if (!function_exists('round_date')) {
         return (new DateTime("$year-$month-$day"))
             ->format('Y-m-d');
     }
+}
 
+if (!function_exists('dateTimeConverter')) {
+    /**
+     * here you can customize how the function interprets input data and how it should return the result
+     * example: you can add 'y'    => 'year'
+     *                      's'    => 'seconds'
+     *                      'u'    => 'microsecond'
+     *
+     * key, can be also a string
+     * example              'us'  => 'microsecond'
+     *
+     * @param $inputs
+     * @return array
+     * @throws Exception
+     */
+    function date_time_converter($inputs) {
+        $dateTimeIndex = ['d' => 'day', 'h' => 'hour', 'm' => 'minutes'];
+        $pattern = "#(([0-9]+)([a-z]+))#";
+        $r = preg_match_all($pattern, $inputs, $matches);
+
+        if ($r === FALSE) {
+            throw new Exception('can not parse input data');
+        }
+
+        if (count($matches) != 4) {
+            throw new Exception('something wrong with input data');
+        }
+
+        $dateI = $matches[2]; // contains number
+        $dates = $matches[3]; // contains char or string
+        $result = array();
+        for ($i=0 ; $i<count ($dates) ; $i++) {
+            if(!array_key_exists($dates[$i], $dateTimeIndex)) {
+                throw new Exception ("dateTimeIndex is not configured properly, please add this index : [" . $dates[$i] . "]");
+            }
+
+            $result[$dateTimeIndex[$dates[$i]]] = (int)$dateI[$i];
+        }
+
+        return $result;
+    }
 }
