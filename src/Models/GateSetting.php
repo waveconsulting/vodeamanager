7e9afddd24181b17a1ce22237c2a046528e7b991
Yudi Hertanto <yudi@vodea.id>
2020-11-06 11:46:15 +0700

    allow nullable gate setting permission

--- src/Models/GateSetting.php
+++ src/Models/GateSetting.php
@@ -17,7 +17,7 @@ class GateSetting extends BaseModel
     protected $validationRules = [
         'role_id' => 'required_without:user_id|exists:roles,id,deleted_at,NULL',
         'valid_from' => 'required|date_format:Y-m-d',
-        'permission_ids' => 'required|array|min:1',
+        'permission_ids' => 'nullable|array',
         'permission_ids.*' => 'required|exists:permissions,id,deleted_at,NULL',
     ];
 
