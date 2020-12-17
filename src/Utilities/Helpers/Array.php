a79769c9d65eb619a42c31dc2553047aa9b6e8e3
Yudi Hertanto <y.hertanto17@gmail.com>
2020-12-17 15:05:10 +0700

    add arr not equal

--- src/Utilities/Helpers/Array.php
+++ src/Utilities/Helpers/Array.php
@@ -19,6 +19,23 @@ if (!function_exists('arr_key_equal')) {
     }
 }
 
+if (!function_exists('arr_key_not_equal')) {
+    /**
+     * @param array $array
+     * @param string $key
+     * @param $value
+     *
+     * @return bool
+     */
+    function arr_key_not_equal(array $array, string $key, $value) {
+        if (is_array($value)) {
+            return !in_array($value, Arr::get($array, $key));
+        }
+
+        return Arr::get($array, $key) != $value;
+    }
+}
+
 if (!function_exists('arr_strict')) {
     /**
      * @param $value
