c2ed5bd4afab148245cb6c8f7343a3dca0dfdceb
Yudi Hertanto <y.hertanto17@gmail.com>
2021-03-25 10:57:53 +0700

    add with resource

--- src/Utilities/Models/BaseView.php
+++ src/Utilities/Models/BaseView.php
@@ -3,32 +3,12 @@
 namespace Vodeamanager\Core\Utilities\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Vodeamanager\Core\Http\Resources\BaseResource;
 use Vodeamanager\Core\Utilities\Traits\WithLabel;
+use Vodeamanager\Core\Utilities\Traits\WithResource;
 use Vodeamanager\Core\Utilities\Traits\WithScope;
 use Vodeamanager\Core\Utilities\Traits\WithSearchable;
 
 abstract class BaseView extends Model
 {
-    use WithSearchable, WithScope, WithLabel;
-
-    protected $indexResource = BaseResource::class;
-    protected $showResource = BaseResource::class;
-    protected $selectResource = BaseResource::class;
-
-    public function getResource()
-    {
-        return $this->indexResource;
-    }
-
-    public function getShowResource()
-    {
-        return $this->showResource;
-    }
-
-    public function getSelectResource()
-    {
-        return $this->selectResource;
-    }
-
+    use WithSearchable, WithScope, WithLabel, WithResource;
 }
