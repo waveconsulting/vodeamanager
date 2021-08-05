ef1a759ca7a402aa32066580f7c52d4dfc7eee97
Yudi Hertanto <y.hertanto17@gmail.com>
2021-08-05 16:11:11 +0700

    add booked number service

--- src/Utilities/Facades/NumberSettingService.php
+++ src/Utilities/Facades/NumberSettingService.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static string generateNumber(string $entity, $date = null, $subjectId = null, int $startCounter = 0)
  * @method static string generateNumberManual(array $components, string $resetType, string $entity, $date = null, $subjectId = null)
+ * @method static string bookNumber(string $entity, $date = null, $subjectId = null)
  *
  * @see \Vodeamanager\Core\Utilities\Services\NumberSettingService
  */
