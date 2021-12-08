a28df90b67320abe811229becb0898edca7e1ffe
Yudi Hertanto <y.hertanto17@gmail.com>
2021-12-08 17:19:24 +0700

    implement with searchable like

--- src/Utilities/Traits/HasSearchableLike.php
+++ src/Utilities/Traits/HasSearchableLike.php
@@ -4,10 +4,8 @@ namespace Vodeamanager\Core\Utilities\Traits;
 
 trait HasSearchableLike
 {
-    protected $searchableLikeColumn = [];
-
     public function getSearchableColumn(): array
     {
-        return $this->searchableLikeColumn;
+        return [];
     }
 }
