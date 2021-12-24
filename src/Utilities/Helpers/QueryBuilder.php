1c127d648ed4d5e153ddb1fd18586ec2a3492cc6
Yudi Hertanto <y.hertanto17@gmail.com>
2021-12-24 11:35:21 +0700

    remove instance

--- src/Utilities/Helpers/QueryBuilder.php
+++ src/Utilities/Helpers/QueryBuilder.php
@@ -1,13 +1,11 @@
 <?php
 
-use Illuminate\Database\Query\Builder;
-
 if (!function_exists('builder_to_sql')) {
     /**
-     * @param Builder $builder
+     * @param $builder
      * @return string
      */
-    function builder_to_sql(Builder $builder) {
+    function builder_to_sql($builder) {
         $query = str_replace(array('?'), array('\'%s\''), $builder->toSql());
 
         return vsprintf($query, $builder->getBindings());
